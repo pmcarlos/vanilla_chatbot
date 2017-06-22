e1f364f4ae850ba6764fe0907b5926af03ede7a9
Carlos <carlos@passaromx.com>
2017-06-22 11:22:33 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Readline = require('readline');
+const rl = Readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
+});
+
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', reply => {
+	console.log(`Dijiste: ${reply}`);	
+	rl.prompt();
+});
